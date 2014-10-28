3fe3b6a72d440c2a2a9aaf432d7dadaf4836fdcb
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-10-28 13:04:09 -0500

    successfully wrote test that failed to import a function we hadn't written. :-)

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,13 @@ from django.test import TestCase
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+from django.core.urlresolvers import resolve
+from django.test import TestCase
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+    """test from page 25 of TDD with Python"""
+
+    def root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
