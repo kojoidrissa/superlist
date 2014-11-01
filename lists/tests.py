ab309a4643e706292167a9ef922de41c6db02c9d
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-10-31 21:56:11 -0500

    fixed the method name in class HomePageTest

--- lists/tests.py
+++ lists/tests.py
@@ -9,6 +9,6 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     """test from page 25 of TDD with Python"""
 
-    def root_url_resolves_to_home_page_view(self):
+    def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
