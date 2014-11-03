4a439bf5086a30a494e80b7a816a3999c2393f55
Kojo Idrissa <kojo.idrissa@gmail.com>
2014-11-02 21:02:22 -0600

    Got things working up to Page 30: The Unit-Test/Code Cycle.

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,8 @@ from django.test import TestCase
 
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -12,3 +14,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.AssertTrue(response.content.startswith(b'<htnml>'))
+        self.AssertIn(b'<title>To-Do lists</title>', response.content)
+        self.AssertTrue(response.content.endswith(b'</html>'))
