534dfbae51027be13d69947e04e22f7950f70750
Kojo Idrissa <kojo.idrissa@gmail.com>
2016-01-10 18:31:25 -0600

    added csrf_token

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -27,6 +28,7 @@ class NewVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
 
+        time.sleep(5)
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
@@ -36,12 +38,13 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
 
+        time.sleep(5)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows),
             "New to-do item did not appear in table"
-        ) 
+        )
 
 
         # There is still a text box inviting her to add another item. She
