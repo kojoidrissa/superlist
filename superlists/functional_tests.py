e7a346fed207db4dae138d0d4b792a3898991ccd
Kojo Idrissa <kojo.idrissa@gmail.com>
2017-09-27 20:25:51 -0500

    finished Chapter 2

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,30 +1,24 @@
 from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-# import unittest
-# import time
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# class NewVisitorTest(unittest.TestCase):
-#     def setUp(self):
-#         self.browser = webdriver.Firefox()
-#         self.browser.implicitly_wait(1)
+    def tearDown(self):
+        self.browser.quit()
 
-#     def tearDown(self):
-#         self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes 
+        # to check out its homepage 
+        self.browser.get('http://localhost:8000')
 
-#     def test_can_start_a_list_and_retrieve_it_later(self):
-#         # Edith has heard about a cool new online to-do app. She goes 
-#         # to check out its homepage 
-#         self.browser.get('http://localhost:8000')
-
-#         # She notices the page title and header mention to-do lists
-#         self.assertIn('To-Do', self.browser.title)
-#         header_text = self.browser.find_element_by_tag_name('h1').text
-#         self.assertIn('To-Do', header_text)
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("Finish This Test!")
+        # header_text = self.browser.find_element_by_tag_name('h1').text
+        # self.assertIn('To-Do', header_text)
 
 #         # She is invited to enter a to-do item straight away
 #         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -65,5 +59,5 @@ assert 'Django' in browser.title
 #         # She visits that URL - her to-do list is still there. 
 #         # Satisfied, she goes back to sleep
 
-# if __name__ == '__main__':
-#     unittest.main(warnings = 'ignore')
+if __name__ == '__main__':
+    unittest.main(warnings = 'ignore')
