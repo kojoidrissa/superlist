c0399434e5e74302a5c492692b49f872f30a88fc
Kojo Idrissa <kojo.idrissa@gmail.com>
2017-10-04 22:01:36 -0500

    unit test/code cycle; Basic view now returns minimal HTML

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -14,6 +14,7 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
+		# print("RESPONSE is: {}".format(response))
 		html = response.content.decode('utf8')
 		self.assertTrue(html.startswith('<html>'))
 		self.assertIn('<title>To-Do lists</title>', html)
