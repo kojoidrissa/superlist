31e80181cf1c4cd550bdc705b6c3c20a9892a12f
Kojo Idrissa <kojo.idrissa@gmail.com>
2017-10-25 19:23:51 -0500

    removed older test pattern

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -15,8 +15,4 @@ class HomePageTest(TestCase):
 		response = self.client.get('/')
 		
 		html = response.content.decode('utf8')
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do lists</title>', html)
-		self.assertTrue(html.endswith('</html>'))
-
 		self.assertTemplateUsed(response, 'home.html')
