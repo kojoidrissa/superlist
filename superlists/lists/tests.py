c7209bfea19c17deede3e02a1d3b9875714c7abe
Kojo Idrissa <kojo.idrissa@gmail.com>
2017-10-25 19:25:21 -0500

    removed unneeded test and remnants of older test approach

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -6,13 +6,6 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
-
-
 	def test_home_page_returns_correct_html(self):
 		response = self.client.get('/')
-		
-		html = response.content.decode('utf8')
 		self.assertTemplateUsed(response, 'home.html')
